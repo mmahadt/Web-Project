78dd16ee7ba92de5d498f65a9dbf7161aa0c2a9b
teddybearofdoom <haroon.ramay@gmail.com>
2016-12-03 00:37:56 +0500

    Form control added
    Init function needs a bit of retouching (I'll get to it later)

--- Trial 01/js/myjs.js
+++ Trial 01/js/myjs.js
@@ -1,15 +1,69 @@
-var slideIndex = 0;
-carousel();
+"Use Strict"
+// Declarations on Top
+// It is a good coding practice to put all declarations at the top of each script or function.
 
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none"; 
+// This will:
+
+// Give cleaner code
+// Provide a single place to look for local variables
+// Make it easier to avoid unwanted (implied) global variables
+// Reduce the possibility of unwanted re-declarations
+
+
+	
+
+
+
+function validateForm() 
+{
+
+
+	var a, b, c, d;
+    a = document.forms["SingupForm"]["FName"].value;
+    b = document.forms["SingupForm"]["LName"].value;
+    c = document.forms["SingupForm"]["Email"].value;
+    d = document.forms["SingupForm"]["Password"].value;
+    if (a == "" || b == "" || c == "" || d == "") 
+    {
+        if (a == "")
+        	alert("First Name field must be filled out");
+        else if (b == "")
+        	alert("Last Name field must be filled out");
+        else if (c == "")
+        	alert("Email field must be filled out");
+        else if (d == "")
+        	alert("Password field must be filled out")
+        return false;
     }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1} 
-    x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 5000); // Change image every 2 seconds
+    else
+    {
+    	alert("Form successfully submitted"); 
+        // onclick(document.getElementById("sub-btn").innerHTML = "<a href='http://www.google.com'> Google </a>");
+    }
 }
 
+function init()
+{
+	var SignUpForm;
+
+	SignUpForm = document.getElementById("my-sub-container-02-signupform");
+	SignUpForm.onsubmit = validateForm;
+
+
+	// var click[3];
+	// click = document.getElementsByClassName[0]("my-sub-container-02-input");
+
+	var formdiv;
+	var input; 
+	formdiv = document.getElementById("my-sub-container-02-signupform");
+	input = document.getElementsByClassName("my-sub-container-02-input");
+
+	for(x=0;x<4;x++)
+	{
+		input[x].addEventListener('focus', function(){formdiv.className = 'focused';});
+		input[x].addEventListener('blur', function(){formdiv.className = '';});
+	}
+	
+}
+
+window.onload = init();
